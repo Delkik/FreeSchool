3e3b15dba5da7446bc8fb37b17d6fcfae2dfb8d3
Dan <54672068+Delkik@users.noreply.github.com>
2025-02-04 14:18:22 -0500

    Finished Navbar component (#1)
    * added icons and small tweaks
    
    * styled navbar

---
+++ components/common/Avatar.tsx
@@ -0,0 +1,25 @@
+// import Image from "next/image";
+import { CircleUserRound } from "lucide-react";
+import styles from "@/modules/common/Avatar.module.css";
+
+interface AvatarProps {
+  width?: string | number;
+  height?: string | number;
+}
+
+export default function Avatar({ width, height }: AvatarProps) {
+  // TODO: either use session or fetch to get avatar image
+
+  // TODO: change to Next/Image
+  const avatarSrc = (
+    <img
+      src={"test-avatar.jpg"}
+      width={width ?? 64}
+      height={height ?? 64}
+      alt="avatar"
+      className={styles.avatar}
+    />
+  );
+
+  return <>{avatarSrc || <CircleUserRound size={width} />}</>;
+}
