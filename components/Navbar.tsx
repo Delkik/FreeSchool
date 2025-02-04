b48e0b4a0e652514d8e03a21d2fe1334945fe45f
delkik <danielelkik@hotmail.com>
2025-02-04 12:49:35 -0500

    added icons and small tweaks

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,45 @@
 import styles from "@/modules/Navbar.module.css";
+import { House } from "lucide-react";
+import Link from "next/link";
+import { ArrowRightFromLine } from "lucide-react";
+import { ArrowLeftFromLine } from "lucide-react";
+import { useState } from "react";
+import { Search } from "lucide-react";
+import { MessageCircle } from "lucide-react";
+import Avatar from "./Avatar";
 
-interface NavbarProps {
-  hey?: string;
-}
+const ICON_SIZE = 48;
 
-export default function Navbar({}: NavbarProps) {
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const openIcon = (
+    <button>
+      {isOpen ? (
+        <ArrowLeftFromLine size={ICON_SIZE} onClick={() => setIsOpen(false)} />
+      ) : (
+        <ArrowRightFromLine size={ICON_SIZE} onClick={() => setIsOpen(true)} />
+      )}
+    </button>
+  );
+
   return (
     <div className={styles.container}>
-      <p>hi</p>
+      <div className={styles.icons_list}>
+        {openIcon}
+        <Link href={""}>
+          <House size={ICON_SIZE} />
+        </Link>
+        <Link href={""}>
+          <Search size={ICON_SIZE} />
+        </Link>
+        <Link href={""}>
+          <MessageCircle size={ICON_SIZE} />
+        </Link>
+        <Link href={""}>
+          <Avatar width={ICON_SIZE} height={ICON_SIZE} />
+        </Link>
+      </div>
     </div>
   );
 }
