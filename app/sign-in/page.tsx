93957e0afcffd896c3e8b23305b8c9e3d57c20b5
delkik <danielelkik@hotmail.com>
2025-02-05 18:12:45 -0500

    added sign in page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import SignInContainer from "@/components/sign-in/SignInContainer";
+import styles from "@/modules/app/sign-in/SignIn.module.css";
+import Container from "@mui/material/Container";
+
 export default function SignIn() {
-  return <>Sign in</>;
+  const rightContainer = <div className={styles.right_container}></div>;
+  const leftContainer = (
+    <Container className={styles.left_container}>
+      <SignInContainer />
+    </Container>
+  );
+
+  return (
+    <div className={styles.page_container}>
+      {leftContainer}
+      {rightContainer}
+    </div>
+  );
 }
--- components/sign-in/SignInContainer.tsx
+++ components/sign-in/SignInContainer.tsx
@@ -1,43 +1,42 @@
-import styles from "@/modules/components/sign-up/SignUpContainer.module.css";
+import styles from "@/modules/components/sign-in/SignInContainer.module.css";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-export default function SignUpContainer() {
+export default function SignInContainer() {
   const router = useRouter();
 
-  const onSignUp = () => {
+  const onSignIn = () => {
     router.push("/dashboard");
   };
 
   return (
     <Box className={styles.container}>
       <div className={styles.heading}>
-        <span className={styles.heading_text}>Welcome!</span>
+        <span className={styles.heading_text}>Welcome back!</span>
+        {/* TODO: change this */}
         <span className={styles.subheading_text}>
-          Sign up to start your child&apos;s homeschooling journey
+          Sign in to continue your child&apos;s homeschooling journey
         </span>
       </div>
       <Box className={styles.form_inputs}>
-        <TextField label="Name" />
         <TextField label="Email" />
         <TextField label="Password" type="password" />
-        <TextField label="Confirm Password" type="password" />
       </Box>
       <Button
         variant="contained"
         size="large"
-        className={styles.sign_up}
-        onClick={onSignUp}
+        className={styles.sign_in}
+        onClick={onSignIn}
       >
-        Sign Up
+        Sign In
       </Button>
-      <span className={styles.sign_in}>
-        Already have an account?
-        <Link href="/sign-in" className="link_text">
-          Sign In
+      <span className={styles.sign_up}>
+        Don&apos;t have an account?
+        <Link href="/sign-up" className="link_text">
+          Sign Up
         </Link>
       </span>
     </Box>
