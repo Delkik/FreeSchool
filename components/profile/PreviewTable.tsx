8d1f1321765dfa10ca8bb27833352a1c4808dfbb
delkik <danielelkik@hotmail.com>
2025-02-10 19:32:18 -0500

    added preview table

---
+++ components/profile/PreviewTable.tsx
@@ -0,0 +1,17 @@
+import Box from "@mui/material/Box";
+import styles from "@/modules/components/profile/PreviewTable.module.css";
+
+interface PreviewTableProps {
+  title: string;
+}
+
+export default function PreviewTable({ title }: PreviewTableProps) {
+  return (
+    <Box className={styles.container}>
+      <div className={styles.table_title}>{title}</div>
+      {/* TODO: list some data */}
+
+      {/* TODO: link to the table page, use query parameters to know data? */}
+    </Box>
+  );
+}
