02a46a09b2c429a57c3ca4ff76424b6471528700
delkik <danielelkik@hotmail.com>
2025-03-11 13:08:12 -0400

    connected login to backend

--- components/sign-in/SignInContainer.tsx
+++ components/sign-in/SignInContainer.tsx
@@ -1,15 +1,44 @@
+"use client";
+
 import styles from "@/modules/components/sign-in/SignInContainer.module.css";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import axios from "axios";
+import { useState } from "react";
+
+interface SignInForm {
+  email: string;
+  password: string;
+}
 
 export default function SignInContainer() {
   const router = useRouter();
 
-  const onSignIn = () => {
-    router.push("/dashboard");
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const [error, setError] = useState();
+
+  const onSubmit: SubmitHandler<SignInForm> = async (data) => {
+    try {
+      const url = `${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`;
+
+      await axios.post(url, data);
+
+      router.push("/dashboard");
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (e: any) {
+      console.error(e);
+      setError(e.response.data.error);
+    }
   };
 
   return (
@@ -21,18 +50,32 @@ export default function SignInContainer() {
           Sign in to continue your child&apos;s homeschooling journey
         </span>
       </div>
-      <Box className={styles.form_inputs}>
-        <TextField label="Email" />
-        <TextField label="Password" type="password" />
-      </Box>
-      <Button
-        variant="contained"
-        size="large"
-        className={styles.sign_in}
-        onClick={onSignIn}
-      >
-        Sign In
-      </Button>
+      <span className={styles.error_text}>{error}</span>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <Box className={styles.form_inputs}>
+          <Controller
+            name="email"
+            control={control}
+            render={({ field }) => <TextField {...field} label="Email" />}
+          />
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => (
+              <TextField {...field} label="Password" type="password" />
+            )}
+          />
+        </Box>
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          className={styles.sign_in}
+          // onClick={onSignIn}
+        >
+          Sign In
+        </Button>
+      </form>
       <span className={styles.sign_up}>
         Don&apos;t have an account?
         <Link href="/sign-up" className="link_text">
